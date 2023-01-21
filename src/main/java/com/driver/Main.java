543ddecbe7a6e87ef07424c06b8d6171733b6de8
praveench97 <praveench1997@gmail.com>
2023-01-21 09:46:42 +0530

    Trail overloading

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    class Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
+    public void main() {
+        Product p = new Product();
+        p.product(5, 6);
+        p.product(1, 2, 3);
+        p.product(2.5,2.0);
+    }
 }
